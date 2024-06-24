f632541ae13bbf01a4ef97358dd6d8342a2b1d81
soundsbyjs <johnscott.schuette7@gmail.com>
2024-06-24 17:56:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sample Cutter is a little tool I made for feature extraction
+
+The purpose of this tool is to create a set of audio files out of one audio file.
+This is so that you can do a long bout of sound design and then cut each sound down into a single file, or you can create AI datasets inside your DAW and then use them for feature extraction!
+
+usage:
+
+1. pip install -r requirements.txt
+2. python3 samplecutter "one long audio file" [options]
---
+++ SampleCutter.py
@@ -0,0 +1,15 @@
+# cut the audio file into equally-spaced parts
+# supply the bpm of the file and a fraction to cut the file into chunks
+
+from fractions import Fractions
+import ffmpeg
+class cutter:
+    interval, bpm, length
+    def cutter(self, bpm, length, numerator, denominator):
+        self.interval =  interval
+        self.bpm = bpm
+        # have to figure out how to parse this vv
+        self.length = fraction(numerator, denominator)
+    def calculate_lengths(self):
+        while (length = length - (bpm * interval) >= 0:
+
